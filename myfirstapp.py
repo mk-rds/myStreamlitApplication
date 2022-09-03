c475ce4d5082f725d6c773740408b7ee431b6e0d
Tan Ming Kit <71838012+mk-rds@users.noreply.github.com>
2022-09-03 10:44:02 +0800

    change link for linkeldn and github repo

--- myfirstapp.py
+++ myfirstapp.py
@@ -11,12 +11,12 @@ readme = st.checkbox("readme first")
 if readme:
 
     st.write("""
-        This is a web app demo using [streamlit](https://streamlit.io/) library. It is hosted on [heroku](https://www.heroku.com/). You may get the codes via [github](https://github.com/richieyuyongpoh/myfirstapp)
+        This is a web app demo using [streamlit](https://streamlit.io/) library. It is hosted on [heroku](https://www.heroku.com/). You may get the codes via [github](https://github.com/mk-rds/myStreamlitApplication)
         """)
 
     st.write ("For more info, please contact:")
 
-    st.write("<a href='https://www.linkedin.com/in/yong-poh-yu/'>Dr. Yong Poh Yu </a>", unsafe_allow_html=True)
+    st.write("<a href='https://www.linkedin.com/in/ming-kit-tan/'>Tan Ming Kit</a>", unsafe_allow_html=True)
 
 option = st.sidebar.selectbox(
     'Select a mini project',
